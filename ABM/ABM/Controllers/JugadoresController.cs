0d1aa754ca984b6b886a04e9259c16a4ad39fcac
Duarte Lu <luisa.isabel.duarte@gmail.com>
2020-05-28 17:31:50 -0300

    Se inicia el proyecto y se agrega la base de datos

---
+++ ABM/ABM/Controllers/JugadoresController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABM.Controllers
+{
+    public class JugadoresController : Controller
+    {
+       
+
+       
+    }
+}
