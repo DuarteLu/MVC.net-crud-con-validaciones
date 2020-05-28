b91a44426821e820bf0486fd85060cee49f1e73f
Duarte Lu <luisa.isabel.duarte@gmail.com>
2020-05-28 17:57:56 -0300

    Se añade la clase JugadoresController.cs

--- ABM/ABM/Controllers/JugadoresController.cs
+++ ABM/ABM/Controllers/JugadoresController.cs
@@ -3,13 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ABM.Servicios;
+using ABM.Datos;
+using System.ComponentModel.DataAnnotations;
 
 namespace ABM.Controllers
 {
     public class JugadoresController : Controller
     {
-       
+        JugadorServicio Jugadores = new JugadorServicio();
 
-       
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Agregar([Bind(Include = "idJugador,Apellido,Edad")] Jugador j)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Jugadores.Agregar(j);
+                return Redirect("/Jugador/Lista");
+            }
+            return View(j);
+        }
+        [HttpGet]
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            return View(Jugadores.ObtenerTodos());
+        }
+        [HttpGet]
+        public ActionResult Modificar(int id)
+        {
+            Jugador j = Jugadores.ObtenerPorId(id);
+            return View(j);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(Jugador j)
+        {
+            Jugadores.Modificar(j);
+            return Redirect("/Jugador/Lista");
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            Jugadores.Eliminar(id);
+            return Redirect("/Jugador/Lista");
+        }
     }
+
+
+}
 }
