fecfb16118d38cd75e1f6f2b11b6658f94ade276
Duarte Lu <luisa.isabel.duarte@gmail.com>
2020-05-28 18:02:56 -0300

    Se agraga carpeta extensiones con las validaciones con DataAnnotations

---
+++ ABM/ABM/Datos/Extensiones/Jugador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABM.Datos
+{
+    [MetadataType(typeof(JugadorMetadata))]
+    public partial class Jugador
+    {
+    }
+}
---
+++ ABM/ABM/Datos/Extensiones/JugadorMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABM.Datos
+{
+    public class JugadorMetadata
+    {
+        [Required(ErrorMessage = "Ingrese el Apellido")]
+        public string Apellido { get; set; }
+
+        [Required]
+        [Range(18, 40)]
+        public int Edad { get; set; }
+    }
+}
